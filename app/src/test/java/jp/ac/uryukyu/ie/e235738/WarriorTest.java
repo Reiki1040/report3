229f8994a1d21f5454a42d5ac5672736b80d66fd
Echigo reiki <e235738@ie.u-ryukyu.ac.jp>
2023-11-21 09:25:04 +0900

    report4.2

--- app/src/test/java/jp/ac/uryukyu/ie/e235738/WarriorTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e235738/WarriorTest.java
@@ -1,18 +1,26 @@
 package jp.ac.uryukyu.ie.e235738;
+import org.checkerframework.checker.units.qual.s;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 public class WarriorTest {
     @Test
     void attackWithWeponSkillTest(){
-        int defaultWarriorHp = 100;
-        Warrior demoWarrior = new Warrior("デモ戦士", defaultWarriorHp, 100);
-        Enemy slime = new Enemy("スライム", 1000, 5);
-        demoWarrior.attackWithWeponSkill(slime);
-        assertEquals(850, slime.getHitpoint());
-        demoWarrior.attackWithWeponSkill(slime);
-        assertEquals(700, slime.getHitpoint());
-        demoWarrior.attackWithWeponSkill(slime);
-        assertEquals(550, slime.getHitpoint());
-}
+        Warrior demoWarrior = new Warrior("デモ戦士", 100, 100);
+        Enemy slime1 = new Enemy("スライム", 150, 0);
+        Enemy slime2 = new Enemy("すらいむ", 150, 0);
+        Enemy slime3 = new Enemy("slime", 150, 0);
+
+        ArrayList<Enemy> slimes = new ArrayList<>();
+        slimes.add(slime1);
+        slimes.add(slime2);
+        slimes.add(slime3);
+
+        for(Enemy e:slimes){
+            demoWarrior.attackWithWeponSkill(e);
+            int nowSlimeHitpoint = e.getHitpoint();
+            assertEquals(0, nowSlimeHitpoint);
+        }
+    }
 }
